6fbc674074509ae83508a3143c711a38bce84888
Sasa Savic Perusina <sasa@bubulazy.com>
2022-12-09 13:16:11 +0000

    Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Header from './componente/Header'
-import Contenido from './componente/Contenido'
-import Futter from './componente/Futter'
-import '/src/Css.css'
+import React from "react";
+import Header from "./componente/Header";
+import Contenido from "./componente/Contenido";
+import Futter from "./componente/Futter";
+import "/src/Css.css";
 
 export default function App() {
   return (
@@ -11,5 +11,5 @@ export default function App() {
       <Contenido />
       <Futter />
     </main>
-  )
-};
+  );
+}
--- src/componente/Futter.jsx
+++ src/componente/Futter.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import img1 from "/src/assets/1.png";
+import img2 from "/src/assets/2.png";
+import img3 from "/src/assets/3.png";
+import img4 from "/src/assets/4.png";
 
 export default function Futter() {
   return (
-    <p>Futter</p>
-  )
+    <div className="futter">
+      <img src={img1} alt="" />
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
+      <img src={img4} alt="" />
+    </div>
+  );
 }
